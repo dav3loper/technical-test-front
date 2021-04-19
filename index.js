2e6a6824bd72a3361ea67c7a4aaedf0129d7135d
celaa <celaa@coches.com>
2021-04-19 16:10:44 +0200

    Remove initialization
    Remove data- attributes to initialize values and click listeners to add to the race

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     buildPillCar("d5f701ab-356f-4307-976d-d64e62d78e93", "Lada 2107", "2000", "60",
         "https://www.coches.com/fotos_historicas/lada/2107-1992/lada_2107-1992_r10.jpg");
 
-    document.querySelectorAll('.pill').forEach(function (element) {
-        element.addEventListener('click', function (event) {
-            putInCompetition(event.currentTarget);
-        })
-    });
-
     //Capturar customEvent lanzado por move: en detail.winner el string del ganador
     document.addEventListener('winner', function (event) {
         alert("El ganador es: " + event.detail.winner)
@@ -20,35 +14,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 });
 
-function startCompetition(){
-    if(readySetGo) {
+function startCompetition() {
+    if (readySetGo) {
         window.scroll(0, 1000)
         const contestants = document.querySelectorAll('.contestant');
         contestants.forEach(function (contestant) {
-            move(contestant, contestant.dataset.name, "right", contestant.dataset.velocity);
+            move(contestant, "XXX", "right", 0);
         })
-    }else{
+    } else {
         alert('No están listos los participantes..');
     }
-}
-
-function putInCompetition(element) {
-    if (document.querySelector('#firstContestant').firstChild === null) {
-        setInPosition(element, '#firstContestant');
-    } else if (document.querySelector('#secondContestant').firstChild === null) {
-        setInPosition(element, '#secondContestant');
-        readySetGo = true;
-    }
-}
-
-function setInPosition(element, positionId) {
-    const imgElement = element.querySelector('.img');
-    let position = document.querySelector(positionId);
-    const imgNode = document.createElement('img');
-    imgNode.classList.add('img');
-    imgNode.setAttribute('src', imgElement.getAttribute('src'));
-    position.dataset.name = element.querySelector('.name').textContent;
-    position.dataset.velocity = element.querySelector('.velocity').textContent;
-    position.appendChild(imgNode);
-
 }
